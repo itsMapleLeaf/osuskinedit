a97a33739daea1e951fe720d4f58637d9502d49e
Enitoni <enitoni@gmail.com>
2017-09-20 13:07:12 +0200

    Stop tracking compiled .js files kthx

--- src/renderer/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const ReactDOM = require("react-dom");
-ReactDOM.render(React.createElement("h1", null, "hello world"), document.getElementById('root'));
