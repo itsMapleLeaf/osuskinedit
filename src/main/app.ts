1a59e19ef6e2ea266d5ba30314cde58e4772acb5
Enitoni <enitoni@gmail.com>
2017-09-20 13:04:31 +0200

    Added BrowserWindow options

--- src/main/app.ts
+++ src/main/app.ts
@@ -4,8 +4,24 @@ import { resolve } from 'path'
 let win: Electron.BrowserWindow
 
 app.on('ready', () => {
-  win = new BrowserWindow()
+  win = new BrowserWindow({
+    show: false,
+    frame: false,
+
+    minHeight: 500,
+    minWidth: 800,
+    
+    backgroundColor: '#000'
+  })
+
   win.loadURL(resolve(__dirname, '../renderer/index.html'))
+
+  win.once('ready-to-show', () => {
+    win.show()
+  })
+
+  // Remove in prod
+  // win.webContents.openDevTools()
 })
 
 app.on('window-all-closed', () => {
