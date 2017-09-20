092b55a358d9fdb36925270dd9b2d6bd5910c4a8
Enitoni <enitoni@gmail.com>
2017-09-20 05:49:32 +0200

    Moved into "src" folder

---
+++ src/renderer/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
+const ReactDOM = require("react-dom");
+ReactDOM.render(React.createElement("h1", null, "succ my dicc"), document.getElementById('root'));
