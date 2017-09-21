5fcc5b0f54783129bece6a81eb7cfee699060443
just nobody <kingdaro@gmail.com>
2017-09-21 03:51:22 -0400

    HOLY ACTUAL SHIT

---
+++ src/renderer/core/stores/RouteStore.ts
@@ -0,0 +1,51 @@
+import { action, computed, observable } from 'mobx'
+import * as React from 'react'
+import { IconType } from 'renderer/common/icons/types'
+import SkinColorsView from 'renderer/core/components/SkinColorsView'
+import SkinElementsView from 'renderer/core/components/SkinElementsView'
+import SkinMetadataView from 'renderer/core/components/SkinMetadataView'
+import SettingsView from 'renderer/settings/components/SettingsView'
+
+interface RouteDefinition {
+  [key: string]: Route
+}
+
+interface Route {
+  component: React.ComponentClass
+  icon: IconType
+}
+
+export const routes: RouteDefinition = {
+  elementList: {
+    component: SkinElementsView,
+    icon: 'navList',
+  },
+  skinData: {
+    component: SkinMetadataView,
+    icon: 'navSkinData',
+  },
+  colors: {
+    component: SkinColorsView,
+    icon: 'navColors',
+  },
+  settings: {
+    component: SettingsView,
+    icon: 'navSettings',
+  },
+}
+
+export class RouteStore {
+  @observable currentRouteName = 'elementList'
+
+  @action
+  setRoute(routeName: string) {
+    this.currentRouteName = routeName
+  }
+
+  @computed
+  get currentRoute() {
+    return routes[this.currentRouteName]
+  }
+}
+
+export default new RouteStore()
