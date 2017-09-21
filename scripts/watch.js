758a318503856f05f693a3e8e6a10bcfc10d0ea0
just nobody <kingdaro@gmail.com>
2017-09-21 18:17:11 -0400

    add error handler on watcher

--- scripts/watch.js
+++ scripts/watch.js
@@ -3,19 +3,21 @@ const config = require('./config')
 
 function watch({ input, output }) {
   const watchOptions = Object.assign({}, input, {
-    output: [output]
+    output: [output],
   })
 
   const watcher = rollup.watch(watchOptions)
 
-  watcher.on('event', event => {
-    if (event.code === 'BUNDLE_START') {
-      console.log(`bundling ${event.input}...`)
-    }
-    if (event.code === 'BUNDLE_END') {
-      console.log('done')
-    }
-  })
+  watcher
+    .on('event', event => {
+      if (event.code === 'BUNDLE_START') {
+        console.log(`bundling ${event.input}...`)
+      }
+      if (event.code === 'BUNDLE_END') {
+        console.log('done')
+      }
+    })
+    .on('error', console.error)
 }
 
 watch(config.mainConfig)
