a7139d523a28a959b0a2440c02693c1c1a26185c
Enitoni <enitoni@gmail.com>
2017-09-22 01:33:46 +0200

    primitive rendering of elements

--- src/renderer/core/components/SkinElementsView/index.tsx
+++ src/renderer/core/components/SkinElementsView/index.tsx
@@ -1,7 +1,25 @@
+import { inject, observer } from 'mobx-react'
 import * as React from 'react'
+import { SkinStore } from 'renderer/skin/stores/SkinStore';
 
-export default class SkinElementsView extends React.Component {
+interface SkinElementsViewProps {
+  skinStore: SkinStore
+}
+
+@inject('skinStore')
+@observer
+export default class SkinElementsView extends React.Component<SkinElementsViewProps> {
+  renderList() {
+    const skin = this.props.skinStore!.skin
+
+    return skin.images.map(skinImage => {
+      return (
+        <img style={{maxWidth: '100%'}} src={skinImage.imagePath} />
+      )
+    })
+  }
+
   render() {
-    return <div>this is skin elements view</div>
+    return <div>{ this.renderList() }</div>
   }
 }
