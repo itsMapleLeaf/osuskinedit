c73fbe50f8192a9bd502bf3d735b6ac7f164c090
just nobody <kingdaro@gmail.com>
2017-09-22 18:11:05 -0400

    added exists/access promisified fs helper

--- src/renderer/common/util/fs.ts
+++ src/renderer/common/util/fs.ts
@@ -4,3 +4,13 @@ import promisify from 'pify'
 export const readdir = promisify(fs.readdir)
 export const readFile = promisify(fs.readFile)
 export const writeFile = promisify(fs.writeFile)
+export const access = promisify(fs.access)
+
+export async function exists(path: string) {
+  try {
+    await access(path, fs.constants.F_OK)
+    return true
+  } catch {
+    return false
+  }
+}
