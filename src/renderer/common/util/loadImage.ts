db88cd92899f9129f4332bd1029ded73fda29762
kingdaro <kingdaro@gmail.com>
2017-09-23 09:49:47 -0400

    added generic async image loading function

---
+++ src/renderer/common/util/loadImage.ts
@@ -0,0 +1,8 @@
+export default function loadImage(src: string) {
+  return new Promise((resolve, reject) => {
+    const image = new Image()
+    image.src = src
+    image.onload = () => resolve(image)
+    image.onerror = event => reject(event.error)
+  })
+}
