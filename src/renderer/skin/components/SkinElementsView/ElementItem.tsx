0e7d5185492670375517041bba764962bfb5facf
kingdaro <kingdaro@gmail.com>
2017-09-24 12:09:52 -0400

    remove logTime import

--- src/renderer/skin/components/SkinElementsView/ElementItem.tsx
+++ src/renderer/skin/components/SkinElementsView/ElementItem.tsx
@@ -4,7 +4,6 @@ import * as React from 'react'
 
 import SkinElement from 'renderer/skin/models/SkinElement'
 
-import { logTime } from '../../../common/util/logTime'
 import './ElementItem.scss'
 
 interface ElementItemProps {
