398314c24b0a8ea20904203ece31055cc6a937f9
kingdaro <kingdaro@gmail.com>
2017-09-24 13:09:02 -0400

    import styles in the correct order

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,13 +1,14 @@
+import './styles/global.scss'
+
+import './styles/flex.scss'
+import './styles/helpers.scss'
+
 import { Provider } from 'mobx-react'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
 import App from 'renderer/core/components/App'
 import stores from 'renderer/stores'
-
-import './styles/flex.scss'
-import './styles/global.scss'
-import './styles/helpers.scss'
 
 function Root() {
   return (
